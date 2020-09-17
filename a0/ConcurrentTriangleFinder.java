c870be2e517f3463b037e9481b3846da58174a08
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-09-17 13:10:50 +0800

    a0/: refactor build and find into same func. 1400-1600ms

--- a0/ConcurrentTriangleFinder.java
+++ a0/ConcurrentTriangleFinder.java
@@ -19,8 +19,8 @@ public class ConcurrentTriangleFinder {
         this.endGate = new CountDownLatch(2);
     }
 
-    /* construct the graph: Adjacency Sets - elems in sets are larger than their key */
-    private void buildGraph() {
+    public String solve() {
+        /* construct the graph: Adjacency Sets - elems in sets are larger than their key */
         BufferedReader reader = new BufferedReader(new StringReader(inputString));
         String s;
         try {
@@ -36,30 +36,24 @@ public class ConcurrentTriangleFinder {
             e.printStackTrace();
         }
 
-//        System.out.println("input graph: ");
-//        for (Map.Entry<String, Set<String>> entry : graph.entrySet()) {
-//            System.out.print(entry.getKey() + ": ");
-//            for (String v2 : entry.getValue()) {
-//                System.out.print(v2 + " ");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println("===== The vertex list is: =====");
-//        for (String s : list) {
-//            System.out.println(s);
-//        }
-//        System.out.println();
-    }
-
-//  private boolean isDescending(String v1, String v2) {
-//		return v1.length() > v2.length() || (v1.length() == v2.length() && v1.compareTo(v2) > 0);
-//	}
-
-    public String getTriangles() {
-        buildGraph();
-
+        //get triangles: multi-threaded using thread-safe StringBuffer
         for (int id = 0; id < 2; ++id) {
-            new Thread(new GetTrianglesTask(id)).start();
+            final int finalId = id;
+            new Thread(() -> {
+                for (int i = finalId; i < list.size(); i += 2) {
+                    String v1 = list.get(i);
+                    for (String v2 : graph.get(v1)) {
+                        if (graph.containsKey(v2)) {
+                            for (String v3 : graph.get(v2)) {
+                                if (graph.get(v1).contains(v3)) {
+                                    triangleStrBuf.append(v1 + " " + v2 + " " + v3 + '\n');
+                                }
+                            }
+                        }
+                    }
+                }
+                endGate.countDown();
+            }).start();
         }
         try {
             endGate.await();
@@ -68,30 +62,4 @@ public class ConcurrentTriangleFinder {
         }
         return triangleStrBuf.toString();
     }
-
-    private class GetTrianglesTask implements Runnable {
-        private final int id;
-
-        private GetTrianglesTask(int id) {
-            this.id = id;
-        }
-
-        @Override
-		public void run() {
-			for (int i = id; i < list.size(); i += 2) {
-				String v1 = list.get(i);
-				for (String v2 : graph.get(v1)) {
-					if (graph.containsKey(v2)) {
-						for (String v3 : graph.get(v2)) {
-							if (graph.get(v1).contains(v3)) {
-								triangleStrBuf.append(v1 + " " + v2 + " " + v3 + '\n');
-							}
-						}
-					}
-				}
-			}
-			endGate.countDown();
-		}
-	}
-
 }
--- a0/Server.java
+++ a0/Server.java
@@ -9,7 +9,6 @@ class Server {
 			System.exit(-1);
 		}
 		int port = Integer.parseInt(args[0]);
-//		int port = 10123;
 
 		ServerSocket ssock = new ServerSocket(port);
 		System.out.println("listening on port " + port);
@@ -36,23 +35,16 @@ class Server {
 						/* get edges from the input as bytes */
 						DataInputStream in = new DataInputStream(clientSocket.getInputStream());
 						int reqDataLen = in.readInt();
-//						System.out.println("received request header, data payload has length " + reqDataLen);
 						byte[] bytes = new byte[reqDataLen];
 						in.readFully(bytes);
-//						System.out.println("input bytes:");
-//						for (Byte b : bytes) {
-//							System.out.println(b);
-//						}
 
 						String inputDataString = new String(bytes, StandardCharsets.UTF_8);
-						//System.out.println(inputDataString);
 
 						/* construct the graph: Adjacency Sets - elems in sets are larger than their key
 						 * and get triangles in the graph and write it to the output
 						 */
 						ConcurrentTriangleFinder finder = new ConcurrentTriangleFinder(inputDataString);
-						String triangleStr = finder.getTriangles();
-//						System.out.println("The triangles are:" + "\n" + triangleStr);
+						String triangleStr = finder.solve();
 
 						/* transfer the result back to the client */
 						byte[] outBytes = triangleStr.getBytes(StandardCharsets.UTF_8);
